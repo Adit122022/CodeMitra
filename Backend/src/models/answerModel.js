b067b162ee149a8a05e7d09604089f4ff167119e
Adit122022 <adity202212@gmail.com>
2025-04-06 13:58:15 +0530

    Fix reference in answer model; change questionId from 'Post' to 'Question'

--- Backend/src/models/answerModel.js
+++ Backend/src/models/answerModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const answerSchema  = new mongoose.Schema({
     body: { type: String, required: true },
-    questionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+    questionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Question', required: true },
     authorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     createdAt: { type: Date, default: Date.now }
 })
